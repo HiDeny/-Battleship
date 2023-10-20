70510929379852d941a38ea7d443680c7ddfeb9f
HiDeny <114344945+HiDeny@users.noreply.github.com>
2023-10-20 19:41:03 +0200

    complete ship factory

--- src/ship.js
+++ src/ship.js
@@ -1,8 +1,21 @@
-const Ship = () => {
+const createShip = (length) => {
+  let health = length;
+
   return {
     length,
-    beenHit,
-    isSunk,
-    hit,
-  }
-}
+    get health() {
+      if (health < 1) return this.isSunk();
+      return health;
+    },
+    isSunk() {
+      if (health < 1) return 'Ship sunk!';
+      return health;
+    },
+    hit() {
+      health -= 1;
+      return 'Got hit';
+    },
+  };
+};
+
+export default createShip;
--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,7 +1,23 @@
-test.todo('Ship: have length');
+import createShip from '../ship';
 
-test.todo('Ship: been hit');
+const testShip = createShip(3);
 
-test.todo('Ship: is sunk');
+test('Ship: have length', () => {
+  expect(testShip.length).toBeGreaterThan(0);
+});
+
+test('Ship: been hit', () => {
+  expect(testShip.health).toBe(testShip.length);
+  expect(testShip.hit()).toBe('Got hit');
+  expect(testShip.health).toBe(testShip.length - 1);
+});
+
+test('Ship: is sunk', () => {
+  expect(testShip.isSunk()).toBe(testShip.health);
+  testShip.hit();
+  testShip.hit();
+  testShip.hit();
+  expect(testShip.isSunk()).toBe('Ship sunk!');
+});
 
 test.todo('Ship: is sunk');
