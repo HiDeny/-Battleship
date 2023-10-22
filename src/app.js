93911bf0c9fa8d814218faa60756a52fbf5c3f10
HiDeny <114344945+HiDeny@users.noreply.github.com>
2023-10-22 16:28:21 +0200

    add simple styles

--- src/app.js
+++ src/app.js
@@ -11,5 +11,11 @@ testGame.setShips();
 console.log(testGame.playRound());
 const uiTestGameboard = createGameboard();
 
-document.body.append(renderBoardUI(testGame.player1.gameboard.board));
-document.body.append(renderBoardUI(testGame.player2.gameboard.board));
+const player1Board = renderBoardUI(testGame.player1.gameboard.board);
+player1Board.classList.add('player1');
+
+const player2Board = renderBoardUI(testGame.player2.gameboard.board);
+player2Board.classList.add('player2');
+
+document.body.append(player1Board);
+document.body.append(player2Board);
